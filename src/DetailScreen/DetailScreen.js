cd3fa0e29ccd5bfe9a48e120456133ea36545d30
John Bowser <john.bow7720@gmail.com>
2020-07-31 16:12:13 -0600

    conditional rendering for details page in progress

--- src/DetailScreen/DetailScreen.js
+++ src/DetailScreen/DetailScreen.js
@@ -35,26 +35,68 @@ export default class DetailScreen extends React.Component {
 
   render() {
     const param = this.props.match.params.search
-    const peopleParam = param === 'people'
-    console.log(peopleParam)
     const {results} = this.context
     const resultItem = this.findResult(results) || {}
 
     //const populateResults = this.renderResultAttributes(resultItem, param)
     //console.log("populate: ", populateResults)
     return (
-      {peopleParam} && (<div className="results-wrapper">
-      <h1>{resultItem.name}</h1>
-      <ul>
-        <li>Height in cm: {resultItem.height  || 'Unknown'}</li>
-        <li>Mass in kg: {resultItem.mass  || 'Unknown'}</li>
-        <li>Hair Color: {resultItem.hair_color || 'Unknown'}</li>
-        <li>Skin Color: {resultItem.skin_color || 'Unknown'}</li>
-        <li>Eye Color: {resultItem.eye_color || 'Unknown'}</li>
-        <li>Birth Year: {resultItem.birth_year || 'Unknown'}</li>
-        <li>Gender: {resultItem.gender || 'Unknown'}</li>
-      </ul> 
-      </div>)
+      <div className="results-wrapper">
+      
+      {param === 'people' && (
+        <>
+          <h2>{resultItem.name}</h2>
+          <ul>
+            <li>Height in cm: {resultItem.height  || 'Unknown'}</li>
+            <li>Mass in kg: {resultItem.mass  || 'Unknown'}</li>
+            <li>Hair Color: {resultItem.hair_color || 'Unknown'}</li>
+            <li>Skin Color: {resultItem.skin_color || 'Unknown'}</li>
+            <li>Eye Color: {resultItem.eye_color || 'Unknown'}</li>
+            <li>Birth Year: {resultItem.birth_year || 'Unknown'}</li>
+            <li>Gender: {resultItem.gender || 'Unknown'}</li>
+          </ul> 
+        </>
+      )}
+
+      {param === 'films' && (
+        <>
+          <h2>{resultItem.title}</h2>
+          <ul>
+            <li>Director: {resultItem.director || 'Unknown'}</li>
+            <li>Producer(s): {resultItem.producer || 'Unknown'}</li>
+            <li>Release Date: {resultItem.release_date || 'Unknown'}</li>
+            <li>Opening Crawl: {resultItem.opening_crawl || 'N/A'}</li>
+          </ul>
+        </>
+      )}
+
+      {param === 'starships' && (
+        <>
+          <h2>{resultItem.name}</h2>
+          <ul>
+          <li>Model: {resultItem.model || 'Unknown'}</li>
+          <li>Manufacturer: {resultItem.manufacturer || 'Unknown'}</li>
+          <li>Cost in credits: {resultItem.cost_in_credits || 'Unknown'}</li>
+          <li>Hyperdrive Rating: {resultItem.hyperdrive_rating || 'Unknown'}</li>
+          <li>Max Speed in Atmosphere: {resultItem.max_atmosphering_speed || 'Unknown'}</li>
+          <li>Length in meters: {resultItem.length || 'Unknown'}</li>
+          <li>Minimum Crew: {resultItem.crew || 'Unknown'}</li>
+          <li>Passenger Capacity: {resultItem.passengers || 'Unknown'}</li>
+          <li>Crew/Passenger Supplies Capacity: {resultItem.consumables || 'Unknown'}</li>
+          <li>Cargo Capacity in metric tons: {resultItem.cargo_capcity || 'Unknown'}</li>
+          </ul>
+        </>
+      )}
+
+      {param === 'vehicles' && (
+        <>
+          <h2>{resultItem.name}</h2>
+          <ul>
+            
+          </ul>
+        </>
+      )}
+      </div>
 
 
     )
