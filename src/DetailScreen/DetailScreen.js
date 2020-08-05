48d96f9a3ccf60d6d0840597005e179bd8a4b79b
John Bowser <john.bow7720@gmail.com>
2020-08-05 10:06:58 -0600

    cleaned up unused commented code

--- src/DetailScreen/DetailScreen.js
+++ src/DetailScreen/DetailScreen.js
@@ -11,36 +11,12 @@ export default class DetailScreen extends React.Component {
       result.id === this.props.match.params.selected
       )
   }
-  //if else statements matching this.props.match.params.search
-  //return JSX for each different type with appropriate attributes
-  //should be done in the render function
-
-  // renderResultAttributes = (resultItem, searchParam) => {
-  //   if(searchParam === 'people') {
-  //     return (
-  //       <div className="results-wrapper">
-  //       <h1>{resultItem.name}</h1>
-  //       <ul>
-  //         <li>Height: {resultItem.height + 'cm' || 'Unknown'}</li>
-  //         <li>Mass: {resultItem.mass + 'kg' || 'Unknown'}</li>
-  //         <li>Hair Color: {resultItem.hair_color || 'Unknown'}</li>
-  //         <li>Skin Color: {resultItem.skin_color || 'Unknown'}</li>
-  //         <li>Eye Color: {resultItem.eye_color || 'Unknown'}</li>
-  //         <li>Birth Year: {resultItem.birth_year || 'Unknown'}</li>
-  //         <li>Gender: {resultItem.gender || 'Unknown'}</li>
-  //       </ul>
-  //     </div>
-  //     )
-  //   }
-  // }
-
+  
   render() {
     const param = this.props.match.params.search
     const {results} = this.context
     const resultItem = this.findResult(results) || {}
 
-    //const populateResults = this.renderResultAttributes(resultItem, param)
-    //console.log("populate: ", populateResults)
     return (
       <div className="results-wrapper">
       
